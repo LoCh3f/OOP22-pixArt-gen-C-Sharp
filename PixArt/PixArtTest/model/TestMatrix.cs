00a3aa2156b63806dba65df585a951104d462a4a
Aldo Visconti <aldovisco20000@gmail.com>
2023-06-21 15:28:29 +0200

    test project refactor

---
+++ PixArt/PixArtTest/model/TestMatrix.cs
@@ -0,0 +1,51 @@
+﻿using System.Drawing;
+using PixArtMain.main.model.matrix;
+
+namespace PixArtTest.model;
+
+
+public class TestMatrix
+{
+    private readonly MatrixImpl _matrix = new(16, 16);
+    private readonly PixelImpl _pixel = new(0, 0, Color.White);
+    private readonly PixelImpl _pixel2 = new(0, 1, Color.White);
+    private readonly PixelImpl _pixel3 = new(0, 2, Color.White);
+    
+    [Fact]
+    public void TestMatrixProperties()
+    {
+        Assert.Equal(16, _matrix.Width);
+        Assert.Equal(16, _matrix.Height);
+        Assert.Equal(256, _matrix.Pixels.Count);
+        Assert.Equal(_pixel, _matrix.Pixels[0]);
+    }
+
+    [Fact]
+    public void TestUpdate()
+    {
+        _pixel.Color = Color.Black;
+        _matrix.UpdatePixel(p => {
+            if (p.PosX == _pixel.PosX && p.PosY == _pixel.PosY)
+            {
+                p.Color = _pixel.Color;
+            }}, _pixel);
+        Assert.Equal(_pixel, _matrix.Pixels[0]);
+        
+        _pixel2.Color = Color.Black;
+        _matrix.UpdatePixel(p => {
+            if (p.PosX == _pixel2.PosX && p.PosY == _pixel2.PosY)
+            {
+                p.Color = _pixel2.Color;
+            }}, _pixel2);
+        Assert.Equal(_pixel2, _matrix.Pixels[1]);
+        
+        _pixel3.Color = Color.Black;
+        _matrix.UpdatePixel(p => {
+            if (p.PosX == _pixel3.PosX && p.PosY == _pixel3.PosY)
+            {
+                p.Color = _pixel3.Color;
+            }}, _pixel3);
+        Assert.Equal(_pixel3, _matrix.Pixels[2]);
+        
+    }
+}
---
+++ PixArt/PixArtTest/model/TestPixel.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+using PixArtMain.main.model.matrix;
+
+namespace PixArtTest.model;
+
+public class TestPixel
+{
+    private readonly PixelImpl _pixel = new(0, 0, Color.White);
+    private readonly PixelImpl _pixel2 = new(0, 1, Color.White);
+    private readonly PixelImpl _pixel3 = new(1, 0, Color.White);
+    private readonly PixelImpl _pixel4 = new(1, 1, Color.White);
+         
+    [Fact]
+    public void TestEquals()
+    {
+        Assert.True(_pixel.Equals(new PixelImpl(0, 0, Color.White)));
+        Assert.False(_pixel.Equals(_pixel2));
+        Assert.False(_pixel.Equals(_pixel3));
+        Assert.False(_pixel.Equals(_pixel4));
+    }
+
+    [Fact]
+    public void TestProperties()
+    {
+        _pixel.Color = Color.Black;
+        Assert.Equal(Color.Black, _pixel.Color);
+        Assert.Equal(0, _pixel.PosX);
+        Assert.Equal(0, _pixel.PosY);
+        Assert.NotEqual(Color.White, _pixel.Color);
+    }
+    
+    
+}
