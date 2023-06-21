867eabb0fb6ff14f619dfcab7a01f5c374a98dcc
Alessandro Ricci <alessandro.ricci38@studio.unibo.it>
2023-06-21 11:56:20 +0200

    Modified Game

--- Ricci/RicciProject/src/main/Game.cs
+++ Ricci/RicciProject/src/main/Game.cs
@@ -2,6 +2,9 @@ namespace RicciProject.main;
 
 public interface Game
 {
+    /* Decided to initialize _gameTimer and _gameType as int because
+        these classes have been made by other team component*/
+
     int _gameTimer { get; }
     int _gameType { get; }
 }
