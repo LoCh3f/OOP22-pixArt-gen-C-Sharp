4954b9a758c9b52ebe5ab713e7a78bcb22d4a71b
Eduard Toni Alexandru <eduardtoni.alexandru@studio.unibo.it>
2023-06-22 15:46:06 +0200

    Implemented test for IProjectBuilder, modified IProject.

--- PixArt/PixArtMain/src/main/model/Alexandru Eduard/project/Project.cs
+++ PixArt/PixArtMain/src/main/model/Alexandru Eduard/project/Project.cs
@@ -1,7 +1,15 @@
-﻿namespace PixArtMain.main.model.Alexandru_Eduard.project;
+﻿using PixArtMain.main.model.Alexandru_Eduard.historyframe;
+using PixArtMain.main.model.matrix;
+
+namespace PixArtMain.main.model.Alexandru_Eduard.project;
 
 public interface IProject
 {
+    string ProjectName { get; }
+    string Path { get; }
+    string FileType { get; }
+    List<IMatrix> Frames { get; }
+    List<HistoryFrameImpl> HistoryFrames { get; }
     void AddNewFrame();
 
     void AddNewHistoryFrame(int index);
---
+++ PixArt/PixArtTest/model/Alexandru Eduard/ProjectBuilderTest.cs
@@ -0,0 +1,15 @@
+﻿using PixArtMain.main.model.Alexandru_Eduard.project;
+
+namespace PixArtTest.model.Alexandru_Eduard;
+
+public class ProjectBuilderTest
+{
+    public void BuilderTest()
+    {
+        IProject p = new ProjectBuilderImpl().ProjectName("primo").Path("/home").FileType(".png").Build();
+        
+        Assert.Equal("primo", p.ProjectName);
+        Assert.Equal("/home", p.Path);
+        Assert.Equal(".png", p.FileType);
+    }
+}
